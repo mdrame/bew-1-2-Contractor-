3a1c13ff9b9e1967cf4b1a8161f39d2422bb38b1
mohammed <mdrame113@gmail.com>
2019-09-28 16:14:46 -0700

    user can create, edit and update playlist

--- app.py
+++ app.py
@@ -28,21 +28,20 @@ def playlists_index():
 @app.route('/playlists/new')
 def playlists_new():
 
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title='New playlist')
 
 
 # create playlist and diret user to index page.
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
     """Submit a new playlist."""
-    playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': request.form.get('videos').split(),
-        'stars': request.form.get('stars')
-        }
+        'videos': request.form.get('videos').split()
+    }
     playlist_id = playlists.insert_one(playlist).inserted_id
-    return redirect(url_for('playlists_index', playlist_id=playlist_id)) # url_for looks for a func name instead of an actual route
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
 
 # showing individual collection by adding _id in parameter.
 @app.route('/playlists/<playlist_id>')
@@ -52,6 +51,25 @@ def playlists_show(playlist_id):
     return render_template('playlists_show.html', playlist=playlist)
 
 
+@app.route('/Playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title="Edit Playlist")
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+
+    update_playlist = {
+                        'title': request.form.get('title'),
+                        'description': request.form.get('description'),
+                        'videos': request.form.get('videos').split(),
+                        'stars': request.form.get('stars')
+    }
+
+    playlists.update_one(   {'_id': ObjectId(playlist_id)},
+                            {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 
 if __name__ == "__main__":
