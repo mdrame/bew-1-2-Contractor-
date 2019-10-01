6d0d2e6c9efafbb446019cf0d470969e9c616c69
mohammed <mdrame113@gmail.com>
2019-10-01 09:44:00 -0700

    adding procfile again

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os 
 
 
-client = MongoClient() # assigning Mongo Connector Class an instance.
-db = client.Playlister # client. is creating a database or using an existing database .
-playlists = db.playlists #? creating a collection with in Playlister database
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+playlists = db.playlists
 
 app = Flask(__name__)
 
@@ -81,5 +83,6 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
